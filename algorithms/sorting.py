2ea0ea647e5811bd13bc9f60018322d7eeeb9476
trishall <trishall@cisco.com>
2020-01-08 10:59:45 -0500

    quicksort algorithm

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -80,9 +80,49 @@ def insertion_sort(arr=None):
     return arr
 
 
+def partition(xs, start, end):
+    """
+    The partioner needed for ``quicksort``
+    Borrowed from:
+    https://www.codementor.io/@garethdwyer/
+        quicksort-tutorial-python-implementation-with-line-by-line-explanation-p9h7jd3r6
+    """
+    follower = leader = start
+    pivot_value = xs[end]
+    print(f"Pivot Value: {pivot_value}")
+    while leader < end:
+        if xs[leader] <= pivot_value:
+            xs[follower], xs[leader] = xs[leader], xs[follower]
+            follower += 1
+        leader += 1
+    xs[follower], xs[end] = xs[end], xs[follower]
+    print(f"partioned: {xs}")
+    return follower
+
+
+def _quicksort(xs, start, end):
+    """The quicksort recursive algorithm."""
+    if start >= end:
+        return
+
+    p = partition(xs, start, end)
+    _quicksort(xs, start, p-1)
+    _quicksort(xs, p+1, end)
+    return xs
+
+
+def quicksort(xs):
+    """The public quicksort method."""
+    a = _quicksort(xs, 0, len(xs) - 1)
+    print(f"Quick Sorted array: {a}")
+    return a
+
+
 if __name__ == '__main__':
     arr = [98, 5, 3, 1, 2, 24, 12, 56, 15]
 
-    # bubble_sort(arr)
-    # selection_sort(arr)
-    insertion_sort(arr)
+    print(f"Sorting: {arr})\n")
+    bubble_sort(arr.copy())
+    selection_sort(arr.copy())
+    insertion_sort(arr.copy())
+    quicksort(arr.copy())
