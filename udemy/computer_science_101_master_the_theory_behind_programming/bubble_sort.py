1984519fb04da0f76cc9608231cb6612ab01f7dd
trishall <trishall@cisco.com>
2020-01-08 12:38:21 -0500

    bubble_sort algorithm made during cs 101 class

---
+++ udemy/computer_science_101_master_the_theory_behind_programming/bubble_sort.py
@@ -0,0 +1,27 @@
+"""
+bubble_sort.py
+~~~~~~~~~~~~~~
+Perform a Bubble Sort
+"""
+
+
+def bubble_sort(arr):
+    """The Bubble Sort algorithm"""
+    is_sorted = True
+    while is_sorted:
+        is_sorted = False
+        for i in range(len(arr) - 1):
+            if arr[i + 1] < arr[i]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                is_sorted = True
+    print(f"Bubble Sort: {arr}")
+    return arr
+
+if __name__ == "__main__":
+    from random import sample
+    arr = sample(range(100), 10)
+    _sorted = sorted(arr.copy())
+
+    print(f"Sorting: {arr})")
+    print(f"Sorted: {_sorted}\n")
+    assert bubble_sort(arr.copy()) == _sorted
