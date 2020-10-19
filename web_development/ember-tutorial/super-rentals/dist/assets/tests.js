278017184a9407b6479973b48483bb444e6773a3
Tristan Hall <33399048+halltristanj@users.noreply.github.com>
2020-10-18 20:47:16 -0400

    Ember tutorial (#5)
    * ember tutorial
    
    * fixing things
    
    * rename fixing
    
    * component-basics
    
    Co-authored-by: Tristan Hall <thall@agari.com>

---
+++ web_development/ember-tutorial/super-rentals/dist/assets/tests.js
@@ -0,0 +1,101 @@
+'use strict';
+
+define("super-rentals/tests/acceptance/super-rentals-test", ["qunit", "@ember/test-helpers", "ember-qunit"], function (_qunit, _testHelpers, _emberQunit) {
+  "use strict";
+
+  (0, _qunit.module)('Acceptance | super rentals', function (hooks) {
+    (0, _emberQunit.setupApplicationTest)(hooks);
+    (0, _qunit.test)('visiting /', async function (assert) {
+      await (0, _testHelpers.visit)('/');
+      assert.equal((0, _testHelpers.currentURL)(), '/');
+      assert.dom('nav').exists();
+      assert.dom('h1').hasText('SuperRentals');
+      assert.dom('h2').hasText('Welcome to Super Rentals!');
+      assert.dom('.jumbo a.button').hasText('About Us');
+      await (0, _testHelpers.click)('.jumbo a.button');
+      assert.equal((0, _testHelpers.currentURL)(), '/about');
+    });
+    (0, _qunit.test)('visiting /about', async function (assert) {
+      await (0, _testHelpers.visit)('/about');
+      assert.equal((0, _testHelpers.currentURL)(), '/about');
+      assert.dom('nav').exists();
+      assert.dom('h1').hasText('SuperRentals');
+      assert.dom('h2').hasText('About Super Rentals');
+      assert.dom('.jumbo a.button').hasText('Contact Us');
+      await (0, _testHelpers.click)('.jumbo a.button');
+      assert.equal((0, _testHelpers.currentURL)(), '/getting-in-touch');
+    });
+    (0, _qunit.test)('visiting /getting-in-touch', async function (assert) {
+      await (0, _testHelpers.visit)('/getting-in-touch');
+      assert.equal((0, _testHelpers.currentURL)(), '/getting-in-touch');
+      assert.dom('nav').exists();
+      assert.dom('h1').hasText('SuperRentals');
+      assert.dom('h2').hasText('Contact Us');
+      assert.dom('a.button').hasText('About');
+      await (0, _testHelpers.click)('.jumbo a.button');
+      assert.equal((0, _testHelpers.currentURL)(), '/about');
+    });
+    (0, _qunit.test)('navigating using the nav-bar', async function (assert) {
+      await (0, _testHelpers.visit)('/');
+      assert.dom('nav').exists();
+      assert.dom('nav a.menu-index').hasText('SuperRentals');
+      assert.dom('nav a.menu-about').hasText('About');
+      assert.dom('nav a.menu-contact').hasText('Contact');
+      await (0, _testHelpers.click)('nav a.menu-about');
+      assert.equal((0, _testHelpers.currentURL)(), '/about');
+      await (0, _testHelpers.click)('nav a.menu-contact');
+      assert.equal((0, _testHelpers.currentURL)(), '/getting-in-touch');
+      await (0, _testHelpers.click)('nav a.menu-index');
+      assert.equal((0, _testHelpers.currentURL)(), '/');
+    });
+  });
+});
+define("super-rentals/tests/integration/components/jumbo-test", ["qunit", "ember-qunit", "@ember/test-helpers"], function (_qunit, _emberQunit, _testHelpers) {
+  "use strict";
+
+  (0, _qunit.module)('Integration | Component | jumbo', function (hooks) {
+    (0, _emberQunit.setupRenderingTest)(hooks);
+    (0, _qunit.test)('it renders the content inside a jumbo header with a tomster', async function (assert) {
+      await (0, _testHelpers.render)(Ember.HTMLBars.template(
+      /*
+        <Jumbo>Hello World</Jumbo>
+      */
+      {
+        id: "YfYp0FPz",
+        block: "{\"symbols\":[],\"statements\":[[7,\"jumbo\",[],[[],[]],[[\"default\"],[{\"statements\":[[1,1,0,0,\"Hello World\"]],\"parameters\":[]}]]]],\"hasEval\":false,\"upvars\":[]}",
+        meta: {}
+      }));
+      assert.dom('.jumbo').exists();
+      assert.dom('.jumbo').hasText('Hello World');
+      assert.dom('.jumbo .tomster').exists();
+    });
+  });
+});
+define("super-rentals/tests/test-helper", ["super-rentals/app", "super-rentals/config/environment", "@ember/test-helpers", "ember-qunit"], function (_app, _environment, _testHelpers, _emberQunit) {
+  "use strict";
+
+  (0, _testHelpers.setApplication)(_app.default.create(_environment.default.APP));
+  (0, _emberQunit.start)();
+});
+define('super-rentals/config/environment', [], function() {
+  var prefix = 'super-rentals';
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(decodeURIComponent(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+});
+
+require('super-rentals/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
